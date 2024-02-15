3f7587b87216ff915c9afa9d7513d796e897a2c5
SutuSebastian <sebiitv@gmail.com>
2024-02-15 14:34:13 +0200

    chore: export `BaseIcon` too, for external use

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { BaseIcon } from "./components/base-icon";
 export { FlowbiteIcons } from "./components/provider";
 export * from "./icons";
 export type { FlowbiteIconProps } from "./types";
