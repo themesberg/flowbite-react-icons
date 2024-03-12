e213f475c3aadeabae8b46d2719b933ffe265322
SutuSebastian <sebiitv@gmail.com>
2024-03-12 22:07:32 +0200

    chore: cleanup comments

--- svgr.template.cjs
+++ svgr.template.cjs
@@ -1,11 +1,4 @@
 function template(variables, { tpl }) {
-  // TODO: replace `<svg>` with `<BaseIcon>`
-
-  // variables.jsx.openingElement.name = "svg";
-  // variables.jsx.closingElement.name = "svg";
-
-  // console.log(JSON.stringify(variables.jsx, null, 2));
-
   return tpl`
 import { forwardRef, type PropsWithoutRef } from "react";
 import { BaseIcon } from "~/components/base-icon";
