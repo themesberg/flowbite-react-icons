7c19d4abb948a66c21e2dd82f0950c109e2b68ba
SutuSebastian <sebiitv@gmail.com>
2024-03-19 11:20:10 +0200

    chore: add dts files to rollup config + small refactor

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,75 @@
 import alias from "@rollup/plugin-alias";
+import dts from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
 import { rollupPluginUseClient } from "rollup-plugin-use-client";
+
+const input = [
+  "src/index.ts",
+  "src/icons/solid/index.ts",
+  "src/icons/outline/index.ts",
+];
+const external = ["react", "react/jsx-runtime"];
 
 /**
  * @type {import('rollup').RollupOptions}
  */
-export default {
-  input: [
-    "src/icons/outline/index.ts",
-    "src/icons/solid/index.ts",
-    "src/index.ts",
-  ],
-  output: [
-    {
-      format: "es",
-      entryFileNames: "[name].mjs",
-      dir: "dist/esm",
-      preserveModules: true,
-      sourcemap: true,
+export default [
+  {
+    input,
+    output: [
+      {
+        format: "es",
+        entryFileNames: "[name].mjs",
+        dir: "dist/esm",
+        preserveModules: true,
+        sourcemap: true,
+      },
+      {
+        format: "cjs",
+        entryFileNames: "[name].cjs",
+        dir: "dist/cjs",
+        preserveModules: true,
+        sourcemap: true,
+      },
+    ],
+    external,
+    plugins: [
+      absoluteImports(),
+      esbuild({
+        sourceMap: false,
+      }),
+      rollupPluginUseClient(),
+    ],
+  },
+  {
+    input: {
+      index: "src/index.ts",
+      "icons/solid/index": "src/icons/solid/index.ts",
+      "icons/outline/index": "src/icons/outline/index.ts",
+      types: "src/types.ts",
     },
-    {
-      format: "cjs",
-      entryFileNames: "[name].cjs",
-      dir: "dist/cjs",
-      preserveModules: true,
-      sourcemap: true,
-      interop: "auto",
-    },
-  ],
-  external: ["react", "react/jsx-runtime"],
-  plugins: [
-    alias({
-      entries: [
-        {
-          find: "~/components/base-icon",
-          replacement: "../../../components/base-icon",
-        },
-      ],
-    }),
-    esbuild({
-      sourceMap: false,
-    }),
-    rollupPluginUseClient(),
-  ],
-};
+    output: [
+      {
+        format: "es",
+        dir: "dist/types",
+      },
+    ],
+    external,
+    plugins: [absoluteImports(), dts()],
+  },
+];
+
+function absoluteImports() {
+  return alias({
+    entries: [
+      {
+        find: "~/components/base-icon",
+        replacement: "../../../components/base-icon",
+      },
+      {
+        find: "~/types",
+        replacement: "../../../types",
+      },
+    ],
+  });
+}
